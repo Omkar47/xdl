efc6c84e0e8854f84aadb37c97ea2715ff5dda16
Sayanth Dinesh <sayanthd@gmail.com>
2020-02-21 12:14:04 +0530

    test 1 of making single config for video and audio
    next one will be --external-downloader

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -130,7 +130,6 @@ async def youtube_dl_call_back(bot, update):
             "-o", download_directory,
             "--external-downloader", "aria2c",
             "--external-downloader-args", '-c -j 16 -x 16 -s 16 -k 5M --uri-selector=adaptive --file-allocation=falloc',
-           # "--verbose"
         ]
     else:
         # command_to_exec = ["youtube-dl", "-f", youtube_dl_format, "--hls-prefer-ffmpeg", "--recode-video", "mp4", "-k", youtube_dl_url, "-o", download_directory]
@@ -153,13 +152,13 @@ async def youtube_dl_call_back(bot, update):
             "-o", download_directory,
             "--external-downloader", "aria2c",
             "--external-downloader-args", '-c -j 16 -x 16 -s 16 -k 5M --uri-selector=adaptive --file-allocation=falloc',
-           # "--verbose"
         ]
     #
     command_to_exec.append("--no-warnings")
     # command_to_exec.append("--quiet")
     command_to_exec.append("--restrict-filenames")
     command_to_exec.append("--embed-thumbnail")
+    # command_to_exec.append("--verbose")
     #
     if "hotstar" in youtube_dl_url:
         command_to_exec.append("--geo-bypass-country")
